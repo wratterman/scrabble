fc175040578fbb11e0425c542adc071f592a5597
wratterman <will.ratterman@gmail.com>
2017-05-19 12:03:09 -0600

    was commiting to the wrong git this whole time

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,18 +1,43 @@
 class Scrabble
 
+  attr_reader :tally
+
+  def initialize
+    @tally = 0
+  end
+
   def score(word)
-    1
+    if word == nil
+      nil_score
+    else
+      score_calculation(word)
+    end
+    return tally
+  end
+
+  def score_calculation(word)
+    word.to_s.each_char do |letter|
+      @tally = tally + point_values[letter.upcase]
+    end
+  end
+
+  def nil_score
+    return tally = 0
   end
 
   def point_values
     {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
+      "A" => 1, "B" => 3, "C" => 3, "D" => 2,
+      "E" => 1, "F" => 4, "G" => 2, "H" => 4,
+      "I" => 1, "J" => 8, "K" => 5, "L" => 1,
+      "M" => 3, "N" => 1, "O" => 1, "P" => 3,
+      "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
+      "U" => 1, "V" => 4, "W" => 4, "X" => 8,
+      "Y" => 4, "Z" => 10, "" => 0, " " => 0,
+      "," => 0, "." => 0, "!" => 0, "?" => 0,
+      "'" => 0, "1" => 0, "2" => 0, "3" => 0,
+      "4" => 0, "5" => 0, "6" => 0, "7" => 0,
+      "8" => 0, "9" => 0, "0" => 0
     }
   end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,9 +5,121 @@ require 'minitest/emoji'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+
+  def test_that_tally_is_default_value_of_zero
+    assert_equal 0, Scrabble.new.tally
+  end
+
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
+  end
+
+  def test_it_can_score_two_letters
+    word = Scrabble.new
+    expected = 4
+    actual = word.score("ab")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_score_two_other_letters
+    word = Scrabble.new
+    expected = 13
+    actual = word.score("kx")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_score_10_letters
+    word = Scrabble.new
+    expected = 18
+    actual = word.score("strawberry")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_return_a_max_score_even_though_this_isnt_a_word
+    word = Scrabble.new
+    expected = 87
+    actual = word.score("abcdefghijklmnopqrstuvwxyz")
+
+    assert_equal expected, actual
+  end
+
+  def test_space_has_a_value_of_zero
+    word = Scrabble.new
+    expected = 0
+    actual = word.score(" ")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_score_two_words
+    word = Scrabble.new
+    expected = 18
+    actual = word.score("will ratterman")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_score_two_other_words
+    word = Scrabble.new
+    expected = 12
+    actual = word.score("cool dude")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_score_multiple_words
+    word = Scrabble.new
+    expected = 30
+    actual = word.score("will ratterman cool dude")
+
+    assert_equal expected, actual
+  end
+
+  def test_nil_score_is_0
+    assert_equal 0, Scrabble.new.score(nil)
+  end
+
+  def test_enter_has_a_score_of_zero
+    word = Scrabble.new
+    expected = 0
+    actual = word.score("")
+
+    assert_equal expected, actual
+  end
+
+  def test_punctuation_has_a_score_of_zero
+    word = Scrabble.new
+    expected = 0
+    actual = word.score("!?.,")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_score_sentences
+    word = Scrabble.new
+    expected = 33
+    actual = word.score("Will Ratterman is a cool dude!")
+
+    assert_equal expected, actual
+  end
+
+  def test_numbers_have_a_score_of_zero
+    word = Scrabble.new
+    expected = 0
+    actual = word.score(1234)
+
+    assert_equal expected, actual
+  end
+
+  def test_it_will_score_words_but_not_numbers
+    word = Scrabble.new
+    expected = 14
+    actual = word.score("4realz")
+
+    assert_equal expected, actual
   end
 end
